b9e66b7f434c9b2587b61acc839ac5ccf344cac2
Mrigyen Sawant <Mrigyen@users.noreply.github.com>
2019-01-04 18:16:33 +0530

    Added working review submission functionality without timestamp.

--- internals.js
+++ internals.js
@@ -4,6 +4,7 @@ var prompt = require('prompt');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var mongodbUrl = 'mongodb://localhost:27017';
+var dbName = 'test';
 
 prompt.start();
 
@@ -12,62 +13,108 @@ module.exports.home = (docs) => {
   console.log("Please select your next option: 'review' 'rate' 'hover' 'exit'");
   prompt.get(["entering"], (err, result) => {
 
-    //exit
     if (result.entering === 'exit') {
       process.exit();
     }
 
-    // review
     else if (result.entering === 'review') {
-      // review(docs);
-      console.log("This feature is under dev");
-      module.exports.home(docs);
+      review(docs);
     }
 
     else if (result.entering === 'rate') {
       console.log("This feature is under dev");
       module.exports.home(docs);
     }
+
     else if (result.entering === 'hover') {
       console.log("This feature is under dev");
       module.exports.home(docs);
     }
+
     else {
       console.log("invalid input, please try again");
       module.exports.home(docs);
     };
-  })
+  });
 };
 
 // review CLI
 function review (docs) {
-  console.log("Please select a hotel to review from the below list:");
+  console.log("Please select a restaurant to review from the below list:");
   console.log("1. Rockk Onn");
   console.log("2. Food of Heaven");
   console.log("3. Aroma");
   console.log("4. Feel It");
   console.log("5. KhanaBot Nights");
-  console.log("Please enter the option number of the hotel you want to review. Or 'back' to go to the previous menu.");
+  console.log("Please enter the option number of the restaurant you want to review. Or 'back' to go to the previous menu.");
   prompt.get(["reviewOption"], (err, result) => {
     if (result.reviewOption === 'back') {
       module.exports.home(docs);
     }
-    else if (result.reviewOption === '1') {
-
+    else if (parseInt(result.reviewOption) >= 1 && parseInt(result.reviewOption) <= 5) {
+      reviewIO(docs, parseInt(result.reviewOption));
+    }
+    else {
+      console.log("Invalid input, please try again");
+      review(docs);
     }
   })
 };
 
 // review I/O
-function reviewIO() {
-  console.log("please enter your review below and press enter to submit");
+function reviewIO(docs, optionNumber) {
+  var option = "test";
+  if(optionNumber === 1) {
+    option = 'rockkonn';
+  }
+  else if (optionNumber === 2) {
+    option = 'foodofheaven';
+  }
+  else if (optionNumber === 3) {
+    option = 'aroma';
+  }
+  else if (optionNumber === 4) {
+    option = 'feelit';
+  }
+  else if (optionNumber === 5) {
+    option = 'khanabotnights'
+  }
+  else {
+    console.log ("optionNumber out of range, please debug");
+    process.exit();
+  }
+
+  console.log(docs[0].username + ", please enter your review below and press enter to submit");
   prompt.get(["review"], (err, result) => {
     if (result.review == "") {
       console.log("Please write an actual review.");
-      reviewIO(/* add same options as before*/);
+      reviewIO(docs, optionNumber);
     }
     else {
-      // mongo submission
-    }
-  })
+      // mongodb submission
+      MongoClient.connect(mongodbUrl, (err, client) => {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+
+        // submission function
+        const submitReview = (db, callback) => {
+          const collection = db.collection(option);
+          collection.insertMany([{
+            username: docs[0].username,
+            review: result.review
+          }], (err, submitresult) => {
+            console.log("Successfully submitted review.");
+            callback(submitresult);
+          });
+        };
+
+        submitReview(db, (reviewDocument) => {
+          console.log("here is the object of the review:");
+          console.log(reviewDocument);
+          module.exports.home(docs);
+          client.close();
+        });
+      });
+    };
+  });
 };
